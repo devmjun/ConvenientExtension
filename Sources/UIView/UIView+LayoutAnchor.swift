afc90818cf9b97c17ad882f7e0daa5dbe1323503
devmjun <dev.mjun@gmail.com>
2019-09-08 20:45:50 +0900

    Fix error

--- Sources/UIView/UIView+LayoutAnchor.swift
+++ Sources/UIView/UIView+LayoutAnchor.swift
@@ -150,7 +150,7 @@ extension UIView {
   
   public func equalToSuperView() -> Self {
     guard let superView = superview else {
-      assert(false, "superView is Not exist, you should check Superview correctly delivere")
+      return self
     }
     return topAnchor(to: superView.topAnchor)
       .bottomAnchor(to: superView.bottomAnchor)
