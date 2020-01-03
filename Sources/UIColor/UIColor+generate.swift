0d9ea24a94ca053de7d46d6d6168b953b27c3883
dev.mjun <dev.mjun@gmail.com>
2020-01-03 11:20:14 +0900

    Change color range

--- Sources/UIColor/UIColor+generate.swift
+++ Sources/UIColor/UIColor+generate.swift
@@ -9,22 +9,41 @@
 import UIKit
 
 extension UIColor {
-    public static var random: UIColor {
-        return UIColor(
-            red: CGFloat.random(in: 0..<256) / 255,
-            green: CGFloat.random(in: 0..<256) / 255,
-            blue: CGFloat.random(in: 0..<256) / 255,
-            alpha: 1)
-    }
+  public static var random: UIColor {
+    return UIColor(
+      red: CGFloat.random(in: 0..<256) / 255,
+      green: CGFloat.random(in: 0..<256) / 255,
+      blue: CGFloat.random(in: 0..<256) / 255,
+      alpha: 1
+    )
+  }
 }
 
 
 extension UIColor {
-    public static func generate(rgb: CGFloat, alpha: CGFloat = 1.0) -> UIColor {
-        return UIColor(red: rgb/256, green: rgb/256, blue: rgb/256, alpha: alpha)
-    }
-    
-    public static func generate(red r: CGFloat, green g: CGFloat, blue b: CGFloat, alpha a: CGFloat = 1.0) -> UIColor {
-        return UIColor(red: r/256, green: g/256, blue: b/256, alpha: a)
-    }
+  public static func generate(
+    rgb: CGFloat,
+    alpha: CGFloat = 1.0
+  ) -> UIColor {
+    return UIColor(
+      red: rgb/255,
+      green: rgb/255,
+      blue: rgb/255,
+      alpha: alpha
+    )
+  }
+  
+  public static func generate(
+    red r: CGFloat,
+    green g: CGFloat,
+    blue b: CGFloat,
+    alpha a: CGFloat = 1.0
+  ) -> UIColor {
+    return UIColor(
+      red: r/255,
+      green: g/255,
+      blue: b/255,
+      alpha: a
+    )
+  }
 }
